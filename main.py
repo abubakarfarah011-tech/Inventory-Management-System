66f3e4bd0374642cc119db36a20a62b016b189a3
Captainsan90 <captainsannn@gmail.com>
2026-03-02 01:34:35 +0300

    cli functionality and some few changes on login side (#6)

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #starts the application and calls the router to begin the cli program flow
+
+# main.py
+
+from cli.router import Router
+
+if __name__ == "__main__":
+    app = Router()
+    app.start()
---
+++ users/__init__.py
@@ -0,0 +1,2 @@
+from .user_model import User
+from .user_service import login, register, update_password
